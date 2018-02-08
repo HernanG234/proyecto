6846280fbd2cac9bc8506e7f10e8dcd740ebe904
Hernán Gonzalez <hernan@vanguardiasur.com.ar>
2018-02-08 18:44:17 -0300

    proyecto: Update Script.sh with new features
    Signed-off-by: Hernán Gonzalez <hernan@vanguardiasur.com.ar>

--- Script.sh
+++ Script.sh
@@ -5,16 +5,77 @@
 ./test FAST BRISK
 ./test FAST FREAK
 ./test FAST LDB
+./test FAST LATCHK
+./test FAST LATCH
+./test FAST BAFT
+./test FAST BOLD
 
 ./test ORB BRIEF
 ./test ORB ORB
 ./test ORB BRISK
 ./test ORB FREAK
 ./test ORB LDB
+./test ORB LATCHK
+./test ORB LATCH
+./test ORB BAFT
+./test ORB BOLD
+
+./test BRISK BRIEF
+./test BRISK ORB
+./test BRISK BRISK
+./test BRISK FREAK
+./test BRISK LDB
+./test BRISK LATCHK
+./test BRISK LATCH
+./test BRISK BAFT
+./test BRISK BOLD
+
+./test AGAST BRIEF
+./test AGAST ORB
+./test AGAST BRISK
+./test AGAST FREAK
+./test AGAST LDB
+./test AGAST LATCHK
+./test AGAST LATCH
+./test AGAST BAFT
+./test AGAST BOLD
 
 ./test GFTT BRIEF
 ./test GFTT ORB
 ./test GFTT BRISK
 ./test GFTT FREAK
 ./test GFTT LDB
+./test GFTT LATCHK
+./test GFTT LATCH
+./test GFTT BAFT
+./test GFTT BOLD
 
+./test BAFT BRIEF
+./test BAFT ORB
+./test BAFT BRISK
+./test BAFT FREAK
+./test BAFT LDB
+./test BAFT LATCHK
+./test BAFT LATCH
+./test BAFT BAFT
+./test BAFT BOLD
+
+./test LOCKY BRIEF
+./test LOCKY ORB
+./test LOCKY BRISK
+./test LOCKY FREAK
+./test LOCKY LDB
+./test LOCKY LATCHK
+./test LOCKY LATCH
+./test LOCKY BAFT
+./test LOCKY BOLD
+
+./test LOCKYS BRIEF
+./test LOCKYS ORB
+./test LOCKYS BRISK
+./test LOCKYS FREAK
+./test LOCKYS LDB
+./test LOCKYS LATCHK
+./test LOCKYS LATCH
+./test LOCKYS BAFT
+./test LOCKYS BOLD
