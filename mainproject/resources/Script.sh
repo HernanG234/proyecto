292069bd82dde0a0f220fdefd0b0d2f83731b3eb
Hernán Gonzalez <hernan@vanguardiasur.com.ar>
2018-04-11 18:11:51 -0300

    proyecto: resources: Update Script.sh
    Signed-off-by: Hernán Gonzalez <hernan@vanguardiasur.com.ar>

--- mainproject/resources/Script.sh
+++ mainproject/resources/Script.sh
@@ -1,81 +1,83 @@
 #!bin/bash
 
-./Features FAST BRIEF
-./Features FAST ORB
-./Features FAST BRISK
-./Features FAST FREAK
-./Features FAST LDB
-./Features FAST LATCHK
-./Features FAST LATCH
-./Features FAST BAFT
-./Features FAST BOLD
+./Features FAST BRIEF GMS
+./Features FAST ORB GMS
+./Features FAST BRISK GMS
+./Features FAST FREAK GMS
+./Features FAST LDB GMS
+./Features FAST LATCHK GMS
+./Features FAST LATCH GMS
+./Features FAST BAFT GMS
+./Features FAST BOLD GMS
 
-./Features ORB BRIEF
-./Features ORB ORB
-./Features ORB BRISK
-./Features ORB FREAK
-./Features ORB LDB
-./FeaturesORB LATCHK
-./Features ORB LATCH
-./Features ORB BAFT
-./Features ORB BOLD
+./Features ORB BRIEF GMS
+./Features ORB ORB GMS
+./Features ORB BRISK GMS
+./Features ORB FREAK GMS
+./Features ORB LDB GMS
+./Features ORB LATCHK GMS
+./Features ORB LATCH GMS
+./Features ORB BAFT GMS
+./Features ORB BOLD GMS
 
-./Features BRISK BRIEF
-./Features BRISK ORB
-./Features BRISK BRISK
-./Features BRISK FREAK
-./Features BRISK LDB
-./Features BRISK LATCHK
-./Features BRISK LATCH
-./Features BRISK BAFT
-./Features BRISK BOLD
+./Features BRISK BRIEF GMS
+./Features BRISK ORB GMS
+./Features BRISK BRISK GMS
+./Features BRISK FREAK GMS
+./Features BRISK LDB GMS
+./Features BRISK LATCHK GMS
+./Features BRISK LATCH GMS
+./Features BRISK BAFT GMS
+./Features BRISK BOLD GMS
 
-./Features AGAST BRIEF
-./Features AGAST ORB
-./Features AGAST BRISK
-./Features AGAST FREAK
-./Features AGAST LDB
-./Features AGAST LATCHK
-./Features AGAST LATCH
-./Features AGAST BAFT
-./Features AGAST BOLD
+./Features AGAST BRIEF GMS
+./Features AGAST ORB GMS
+./Features AGAST BRISK GMS
+./Features AGAST FREAK GMS
+./Features AGAST LDB GMS
+./Features AGAST LATCHK GMS
+./Features AGAST LATCH GMS
+./Features AGAST BAFT GMS
+./Features AGAST BOLD GMS
 
-./Features GFTT BRIEF
-./Features GFTT ORB
-./Features GFTT BRISK
-./Features GFTT FREAK
-./Features GFTT LDB
-./Features GFTT LATCHK
-./Features GFTT LATCH
-./Features GFTT BAFT
-./Features GFTT BOLD
+./Features GFTT BRIEF GMS
+./Features GFTT ORB GMS
+./Features GFTT BRISK GMS
+./Features GFTT FREAK GMS
+./Features GFTT LDB GMS
+./Features GFTT LATCHK GMS
+./Features GFTT LATCH GMS
+./Features GFTT BAFT GMS
+./Features GFTT BOLD GMS
 
-./Features BAFT BRIEF
-./Features BAFT ORB
-./Features BAFT BRISK
-./Features BAFT FREAK
-./Features BAFT LDB
-./Features BAFT LATCHK
-./Features BAFT LATCH
-./Features BAFT BAFT
-./Features BAFT BOLD
+./Features BAFT BRIEF GMS
+./Features BAFT ORB GMS
+./Features BAFT BRISK GMS
+./Features BAFT FREAK GMS
+./Features BAFT LDB GMS
+./Features BAFT LATCHK GMS
+./Features BAFT LATCH GMS
+./Features BAFT BAFT GMS
+./Features BAFT BOLD GMS
 
-./Features LOCKY BRIEF
-./Features LOCKY ORB
-./Features LOCKY BRISK
-./Features LOCKY FREAK
-./Features LOCKY LDB
-./Features LOCKY LATCHK
-./Features LOCKY LATCH
-./Features LOCKY BAFT
-./Features LOCKY BOLD
+./Features LOCKY BRIEF GMS
+./Features LOCKY ORB GMS
+./Features LOCKY BRISK GMS
+./Features LOCKY FREAK GMS
+./Features LOCKY LDB GMS
+./Features LOCKY LATCHK GMS
+./Features LOCKY LATCH GMS
+./Features LOCKY BAFT GMS
+./Features LOCKY BOLD GMS
 
-./Features LOCKYS BRIEF
-./Features LOCKYS ORB
-./Features LOCKYS BRISK
-./Features LOCKYS FREAK
-./Features LOCKYS LDB
-./Features LOCKYS LATCHK
-./Features LOCKYS LATCH
-./Features LOCKYS BAFT
-./Features LOCKYS BOLD
+./Features LOCKYS BRIEF GMS
+./Features LOCKYS ORB GMS
+./Features LOCKYS BRISK GMS
+./Features LOCKYS FREAK GMS
+./Features LOCKYS LDB GMS
+./Features LOCKYS LATCHK GMS
+./Features LOCKYS LATCH GMS
+./Features LOCKYS BAFT GMS
+./Features LOCKYS BOLD GMS
+
+./Features AKAZE AKAZE GMS
