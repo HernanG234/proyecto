f8a7d2e656100034b9e9fe7d39bea5a52145587e
Emiliano Santano <emsantano@gmail.com>
2018-04-11 18:51:44 -0300

    :proyecto:Added BFM lines to script.sh

--- mainproject/resources/Script.sh
+++ mainproject/resources/Script.sh
@@ -81,3 +81,89 @@
 ./Features LOCKYS BOLD GMS
 
 ./Features AKAZE AKAZE GMS
+
+
+
+
+
+./Features FAST BRIEF BFM
+./Features FAST ORB BFM
+./Features FAST BRISK BFM
+./Features FAST FREAK BFM
+./Features FAST LDB BFM
+./Features FAST LATCHK BFM
+./Features FAST LATCH BFM
+./Features FAST BAFT BFM
+./Features FAST BOLD BFM
+
+./Features ORB BRIEF BFM
+./Features ORB ORB BFM
+./Features ORB BRISK BFM
+./Features ORB FREAK BFM
+./Features ORB LDB BFM
+./Features ORB LATCHK BFM
+./Features ORB LATCH BFM
+./Features ORB BAFT BFM
+./Features ORB BOLD BFM
+
+./Features BRISK BRIEF BFM
+./Features BRISK ORB BFM
+./Features BRISK BRISK BFM
+./Features BRISK FREAK BFM
+./Features BRISK LDB BFM
+./Features BRISK LATCHK BFM
+./Features BRISK LATCH BFM
+./Features BRISK BAFT BFM
+./Features BRISK BOLD BFM
+
+./Features AGAST BRIEF BFM
+./Features AGAST ORB BFM
+./Features AGAST BRISK BFM
+./Features AGAST FREAK BFM
+./Features AGAST LDB BFM
+./Features AGAST LATCHK BFM
+./Features AGAST LATCH BFM
+./Features AGAST BAFT BFM
+./Features AGAST BOLD BFM
+
+./Features GFTT BRIEF BFM
+./Features GFTT ORB BFM
+./Features GFTT BRISK BFM
+./Features GFTT FREAK BFM
+./Features GFTT LDB BFM
+./Features GFTT LATCHK BFM
+./Features GFTT LATCH BFM
+./Features GFTT BAFT BFM
+./Features GFTT BOLD BFM
+
+./Features BAFT BRIEF BFM
+./Features BAFT ORB BFM
+./Features BAFT BRISK BFM
+./Features BAFT FREAK BFM
+./Features BAFT LDB BFM
+./Features BAFT LATCHK BFM
+./Features BAFT LATCH BFM
+./Features BAFT BAFT BFM
+./Features BAFT BOLD BFM
+
+./Features LOCKY BRIEF BFM
+./Features LOCKY ORB BFM
+./Features LOCKY BRISK BFM
+./Features LOCKY FREAK BFM
+./Features LOCKY LDB BFM
+./Features LOCKY LATCHK BFM
+./Features LOCKY LATCH BFM
+./Features LOCKY BAFT BFM
+./Features LOCKY BOLD BFM
+
+./Features LOCKYS BRIEF BFM
+./Features LOCKYS ORB BFM
+./Features LOCKYS BRISK BFM
+./Features LOCKYS FREAK BFM
+./Features LOCKYS LDB BFM
+./Features LOCKYS LATCHK BFM
+./Features LOCKYS LATCH BFM
+./Features LOCKYS BAFT BFM
+./Features LOCKYS BOLD BFM
+
+./Features AKAZE AKAZE BFM
